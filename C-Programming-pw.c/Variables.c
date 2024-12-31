e65a41f11d96ac79f21db1b75ffab7a1f322cfff
Prolayjit-b14 <prolayjitbiswas14112004@gmail.com>
2024-12-31 22:03:03 +0530

    Lecture 1 Day 1

---
+++ C-Programming-pw.c/Variables.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    printf("Hello PW\n");
+
+    int x;
+    x = 5;
+    printf("%d",x);
+    x = 200;
+    printf("\n%d",x);
+    x = 7;
+    printf("\n");
+    printf("%d",x);
+    x = x + 6;
+    printf("\n%d",x);
+    printf("\n");
+    x = x - 3;
+    printf("%d",x);
+    return 0;
+}
